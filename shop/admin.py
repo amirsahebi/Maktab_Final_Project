0cd823e6688bf3454f38271847ee511d34c8c948
Amir Mohammad Sahebi <amir_sahebi8080@yahoo.com>
2022-01-07 07:38:24 +0330

    first phase published!

---
+++ shop/admin.py
@@ -0,0 +1,61 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import *
+from django.utils.html import format_html
+# Register your models here.
+
+
+admin.site.register(Category)
+admin.site.register(Comment)
+admin.site.register(Tag)
+admin.site.register(Cart)
+admin.site.register(CartItem)
+admin.site.register(CustomUser)
+
+
+
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('name','caption','created_at','shop','show_image')
+    list_filter = ('created_at','shop')
+    search_fields = ('name','caption')
+    date_hierarchy = ('created_at')
+
+    # @admin.display(empty_value='-',description="title desc")
+    # def view_title_desc(self, obj):
+    #     if (obj.image):
+    #         print(obj.image.url)
+    #     print(type(obj))
+    #     return format_html(
+    #          '<span style="color: red;">{} {}</span>',
+    #         obj.name,
+    #         obj.caption,
+    #     )
+    #     # return obj.shortdesc obj.title
+    #     # return f'{} {}'
+    @admin.display(empty_value='-',description="show image")
+    def show_image(self, obj):
+        if (obj.image):
+            print(obj.image.url)
+        
+            return format_html(
+                '<img src="{}" width=50 height=50/>',
+                obj.image.url,
+                
+            )
+        return '-'
+    
+    # fields = (('title','shortdesc'),'desc',('owner','status'))
+    # save_on_top =True
+admin.site.register(Product,PostAdmin)
+
+
+
+class ShopAdmin(admin.ModelAdmin):
+    list_display = ('name','status')
+    actions = ['make_published']
+    @admin.action(description='Mark selected stories as published')
+    def make_published(modeladmin, request, queryset):
+        queryset.update(status='Pub')
+admin.site.register(Store,ShopAdmin)
---
+++ shop/models.py
@@ -0,0 +1,109 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.urls import reverse
+from uuslug import slugify
+
+
+# Create your models here.
+
+USER_TYPE = (('Seller', 'seller'), ('Buyer', 'buyer'))
+class CustomUser(AbstractUser):
+    user_type = models.CharField(
+        max_length=10, choices=USER_TYPE, default='Seller')
+class Store(models.Model):
+    STATUS=(
+        ("Published","Pub"),
+        ("NotPublished","NotPub")
+    )
+    status = models.CharField(
+        max_length = 20,
+        choices = STATUS,
+        default = 'NotPublished'
+        )
+    deleted = models.BooleanField(default=False)
+    name = models.CharField(max_length=55)
+    owner = models.ForeignKey('CustomUser',on_delete=models.CASCADE,null=False)
+
+    def __str__(self):
+        return self.name
+
+
+
+class Product(models.Model):
+    shop = models.ForeignKey(Store,on_delete=models.CASCADE)
+    name = models.CharField(max_length=55)
+    image = models.ImageField()
+    caption = models.TextField()
+    category = models.ManyToManyField('Category',related_name='post')
+    tag = models.ManyToManyField('Tag',blank=True,related_name='post')
+    created_at = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(null=False,unique=True)
+
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('post_detail', kwargs={'slug': self.slug})
+
+    def save(self, *args, **kwargs):
+        self.name = slugify(self.name, self)
+        super(Product, self).save(*args, **kwargs)
+
+
+class Comment(models.Model):
+    name = models.CharField(max_length=55)
+    content = models.TextField()
+    product = models.ForeignKey(Product,default=None,on_delete=models.CASCADE,related_name='comment')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    parent = models.ForeignKey('self',blank=True,null=True,default=None,related_name='nested_category',on_delete=models.CASCADE)
+    name = models.CharField(max_length=55)
+    created_at = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(null=False,unique=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('category_detail', kwargs={'slug': self.slug})
+
+class Tag(models.Model):
+    title = models.CharField(max_length=255)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def get_absolute_url(self):
+        return reverse('tag_detail', kwargs={'id': self.pk})
+    
+    def __str__(self):
+        return self.title
+
+class Cart(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE,null=True)
+    is_paid = models.BooleanField(default=False)
+    accepted = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    
+    def get_absolute_url(self):
+        return reverse('orderdetail', kwargs={'id': self.pk})
+    
+    def __str__(self):
+        return self.user.username
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE,related_name='cartitem')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()
+
+       
+
+    def __str__(self):
+        return self.product.name
+
